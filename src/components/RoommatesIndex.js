b5caa04845613b7ad6518fd86ad9cb456cbbe3b1
imid <indranil2412@gmail.com>
2023-10-07 16:25:03 +0530

    initial setup

---
+++ src/components/RoommatesIndex.js
@@ -0,0 +1,8 @@
+
+export default function RoommatesIndex(){
+    return(
+        <div className="container-fluid">
+           <h1 className="mt-25">The Roommates Project Website will be here</h1>
+        </div>
+    );
+}
